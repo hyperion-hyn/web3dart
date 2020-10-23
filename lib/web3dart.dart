88a41f9227dc27084ce1c868e104f0f74cb3ae0c
moo <moyaying88@gmail.com>
2020-10-23 18:08:40 +0800

    add bech32 lib

--- lib/web3dart.dart
+++ lib/web3dart.dart
@@ -1,7 +1,9 @@
 library web3dart;
 
 import 'dart:async';
+import 'dart:convert';
 import 'dart:typed_data';
+import 'package:convert/convert.dart';
 
 import 'package:collection/collection.dart';
 import 'package:http/http.dart';
@@ -21,15 +23,31 @@ export 'contracts.dart';
 export 'credentials.dart';
 
 part 'src/core/amount.dart';
+
 part 'src/core/block_number.dart';
+
 part 'src/core/client.dart';
+
 part 'src/core/expensive_operations.dart';
+
 part 'src/core/filters.dart';
+
 part 'src/core/sync_information.dart';
+
 part 'src/core/transaction.dart';
+
 part 'src/core/transaction_information.dart';
+
 part 'src/core/transaction_signer.dart';
 
 part 'src/atlas/microstaking_message.dart';
+
 part 'src/atlas/restaking_message.dart';
+
 part 'src/atlas/message.dart';
+
+part 'src/bech32/bech32.dart';
+
+part 'src/bech32/exceptions.dart';
+
+part 'src/bech32/segwit.dart';
